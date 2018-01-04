372c1900fe86d88ffbe76a636c2d7912118c502e
JiangFei <jfliuyun@163.com>
2018-01-04 09:17:24 +0800

    Add files via upload

---
+++ IConnect.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdeSAAS.DataService
+{
+    public interface IConnect : IDisposable
+    {
+        MongoDatabaseSettings DatabaseSettings
+        {
+            get;
+        }
+
+        IMongoCollection<T> Collection<T>(string collectionName);
+    }
+}
